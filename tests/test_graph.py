bccbf4c1d70b8670c8d81c411626eed8747c87f4
Anelya Dusaeva <an.dusaeva+git@gmail.com>
2023-02-25 18:40:33 +0300

    исправлены отступы

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -6,12 +6,20 @@ import cfpq_data as cd
 
 from project.graph_info import *
 
-graphs = [cd.graph_from_csv(cd.download("skos")), cd.graph_from_csv(cd.download("wc")), cd.graph_from_csv(cd.download("generations"))]
+graphs = [
+    cd.graph_from_csv(cd.download("skos")),
+    cd.graph_from_csv(cd.download("wc")),
+    cd.graph_from_csv(cd.download("generations")),
+]
+
 
 def is_graphs_equal(g, g1):
-    return g.edges == g1.edges \
-        and g.nodes == g1.nodes \
+    return (
+        g.edges == g1.edges
+        and g.nodes == g1.nodes
         and get_edges_labels(g) == get_edges_labels(g1)
+    )
+
 
 def test_load_graph():
     names = ["skos", "wc", "generations"]
@@ -19,9 +27,10 @@ def test_load_graph():
         loaded_graph = load_graph(names[i])
         assert is_graphs_equal(graphs[i], loaded_graph)
 
+
 def test_save_graph():
     save_graph(graphs[0], "skos")
-    assert "skos" in os.listdir('.')
+    assert "skos" in os.listdir(".")
     os.remove("skos")
 
 
@@ -34,15 +43,17 @@ def test_get_edges_number():
     for graph in graphs:
         assert get_edges_number(graph) == graph.number_of_edges()
 
+
 def test_get_edges_labels():
-    assert get_edges_labels(graphs[1]) == {'d', 'a'}
+    assert get_edges_labels(graphs[1]) == {"d", "a"}
+
 
 def test_build_two_cycle_graph():
     n = 8
     m = 10
     labels = ("A", "B")
     my_graph = build_two_cycle_graph(n, m, labels)
-    sample_graph = cd.labeled_two_cycles_graph(n, m , labels=labels)
+    sample_graph = cd.labeled_two_cycles_graph(n, m, labels=labels)
     assert is_graphs_equal(my_graph, sample_graph)
 
 
@@ -52,7 +63,7 @@ def test_build_and_save_two_cycle_graph():
     labels = ("A", "B")
     filename = "saved_graph"
     my_graph = build_and_save_two_cycle_graph(n, m, labels, filename)
-    sample_graph = cd.labeled_two_cycles_graph(n, m , labels=labels)
+    sample_graph = cd.labeled_two_cycles_graph(n, m, labels=labels)
     assert is_graphs_equal(my_graph, sample_graph)
-    assert "saved_graph" in os.listdir('.')
+    assert "saved_graph" in os.listdir(".")
     os.remove("saved_graph")
